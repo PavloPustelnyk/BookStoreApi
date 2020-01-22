eb2656edf693757a6c6fe3e9fa19f18a485851bd
pavlo.pustelnyk.pz.2017@lpnu.ua <pavlo.pustelnyk.pz.2017@lpnu.ua>
2020-01-22 12:48:38 +0200

    Add common interface for CRUD service

---
+++ BookStore.Infrastructure.Services/Interfaces/ICrudService.cs
@@ -0,0 +1,30 @@
+﻿using BookStore.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.Infrastructure.Services.Interfaces
+{
+    public interface ICrudService<TEntity> where TEntity: BaseEntity    
+    {
+        IQueryable<TEntity> GetAll();
+
+        Task<IEnumerable<TEntity>> GetAllAsync();
+
+        Task<TEntity> GetByIdAsync(int id);
+
+        Task CreateAsync(TEntity entity);
+
+        Task CreateRangeAsync(IEnumerable<TEntity> entities);
+
+        Task UpdateAsync(TEntity entity);
+
+        Task UpdateRangeAsync(IEnumerable<TEntity> entities);
+
+        Task DeleteAsync(TEntity entity);
+
+        Task DeleteRangeAsync(IEnumerable<TEntity> entities);
+    }
+}
