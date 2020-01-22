f6aa1e0517bc1d8c5976d00d55505486512c451d
pavlo.pustelnyk.pz.2017@lpnu.ua <pavlo.pustelnyk.pz.2017@lpnu.ua>
2020-01-22 12:54:47 +0200

    Configure API services, add authentication and authorization

--- BookStore/Startup.cs
+++ BookStore/Startup.cs
@@ -2,16 +2,30 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
+using BookStore.Infrastructure.ApiContext;
+using BookStore.WebAPI.MappingProfiles;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using BookStore.Infrastructure.Helpers;
+using BookStore.Infrastructure.Services.Interfaces;
+using BookStore.Infrastructure.Services;
+using Microsoft.OpenApi.Models;
+using BookStore.WebAPI.CustomMiddleware;
+using Serilog.Extensions.Logging;
 
-namespace BookStoreApi
+namespace BookStore.WebAPI
 {
     public class Startup
     {
@@ -25,23 +39,50 @@ namespace BookStoreApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<BookStoreDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("AppDBConnection")));
+
+            services.ConfigureInfrastructureServices();
+
             services.AddControllers();
-            
+
+            services.ConfigureAuthentication(Configuration);
+
+            services.ConfigureCors();
+
+            services.ConfigureSwagger();
+
+            services.ConfigureMapping();
+
+            services.
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILoggerFactory loggerFactory)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+
+                app.UseSwagger();
+
+                app.UseSwaggerUI(options =>
+                {
+                    options.SwaggerEndpoint("/swagger/v1/swagger.json", "BookStore API V1");
+                });
             }
+
+            loggerFactory.AddFile("Logs/log-{Date}.txt");
 
             app.UseHttpsRedirection();
 
             app.UseRouting();
+            
+            app.UseAuthentication();
 
             app.UseAuthorization();
+
+            app.UseMiddleware<ErrorHandlingMiddleware>();
 
             app.UseEndpoints(endpoints =>
             {
