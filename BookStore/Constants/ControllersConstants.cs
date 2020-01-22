086031d8bc34f8aa78f00bc53502e6c7ef735470
pavlo.pustelnyk.pz.2017@lpnu.ua <pavlo.pustelnyk.pz.2017@lpnu.ua>
2020-01-22 12:52:20 +0200

    Add constants for controllers

---
+++ BookStore/Constants/ControllersConstants.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.WebAPI.Constants
+{
+    public class ControllersConstants
+    {
+        public const int ItemsOnPageCount = 30;
+    }
+}
