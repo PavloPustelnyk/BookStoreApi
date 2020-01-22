a8a077394418b55f7feee5d4b4ca7cf6927b46e2
pavlo.pustelnyk.pz.2017@lpnu.ua <pavlo.pustelnyk.pz.2017@lpnu.ua>
2020-01-22 12:45:28 +0200

    Add BookStore constants for entites

---
+++ BookStore.Domain/Constants/DbColumnConstraints.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookStore.Domain.Constants
+{
+    public static class DbColumnConstraints
+    {
+        public const int FirstNameLength = 100;
+
+        public const int LastNameLength = 100;
+
+        public const int EmailLength = 320;
+
+        public const int PasswordLength = 128;
+
+        public const int RoleLength = 50;
+
+        public const int BookCategoryLength = 100;
+
+        public const int BookCommentLength = 3000;
+
+        public const int BookTitleLength = 100;
+
+        public const int DescriptionLength = 3000;
+
+        public const int ImageSize = 5 * 1024 * 1024;
+    }
+}
---
+++ BookStore.Domain/Constants/UserRoles.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookStore.Domain.Constants
+{
+    public static class UserRoles
+    {
+        public const string UserRole = "user";
+
+        public const string AdminRole = "admin";
+
+        public const string AllUsersRole = "user, admin";
+    }
+}
