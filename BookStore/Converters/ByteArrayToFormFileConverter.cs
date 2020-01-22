58a6f2edd79d7481d0b671e4b6c5480bf9ae0ed3
pavlo.pustelnyk.pz.2017@lpnu.ua <pavlo.pustelnyk.pz.2017@lpnu.ua>
2020-01-22 12:50:44 +0200

    Add converters for Automapper

---
+++ BookStore/Converters/ByteArrayToFormFileConverter.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.WebAPI.Converters
+{
+    public class ByteArrayToFormFileConverter : ITypeConverter<byte[], IFormFile>
+    {
+        public IFormFile Convert(byte[] source, IFormFile destination, ResolutionContext context)
+        {
+            if (source == null || source.Length == 0)
+            {
+                return null;
+            }
+
+            using var ms = new MemoryStream(source);
+            return new FormFile(ms, 0, source.Length, "name", "fileName");
+        }
+    }
+}
---
+++ BookStore/Converters/FormFileToByteArrayConverter.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.WebAPI.Converters
+{
+    public class FormFileToByteArrayConverter : ITypeConverter<IFormFile, byte[]>
+    {
+        public byte[] Convert(IFormFile source, byte[] destination, ResolutionContext context)
+        {
+            if (source == null || source.Length == 0)
+            {
+                return null;
+            }
+
+            using var ms = new MemoryStream();
+            source.CopyTo(ms);
+            return ms.ToArray();
+        }
+    }
+}
