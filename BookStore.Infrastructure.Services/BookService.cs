ac0c1927790d1ffe050c8c431fef483a03fa48f1
pavlo.pustelnyk.pz.2017@lpnu.ua <pavlo.pustelnyk.pz.2017@lpnu.ua>
2020-01-23 16:33:01 +0200

    Add GetBooksByPartialTitle method, add atomic CreateBookWithCategories and UpdateBookWithCategories methods

--- BookStore.Infrastructure.Services/BookService.cs
+++ BookStore.Infrastructure.Services/BookService.cs
@@ -16,6 +16,23 @@ namespace BookStore.Infrastructure.Services
         {
         }
 
+        public async Task CreateBookWithCategoriesAsync(Book book, int[] categoriesId)
+        {
+            using (var transaction = await dbContext.Database.BeginTransactionAsync())
+            {
+                await dbContext.Books.AddAsync(book);
+                await dbContext.SaveChangesAsync();
+
+                if (categoriesId != null)
+                {
+                    AddBookCategories(book.Id, categoriesId);
+                    await dbContext.SaveChangesAsync();
+                }
+
+                await transaction.CommitAsync();
+            }
+        }
+
         public async Task<int> GetBookLikesCountAsync(int id)
         {
             var book = await GetAll().Where(b => b.Id == id).Include(b => b.LikedBy).FirstOrDefaultAsync();
@@ -26,6 +43,38 @@ namespace BookStore.Infrastructure.Services
             }
 
             return book.LikedBy.Count;
+        }
+
+        public async Task<ICollection<Book>> GetBooksByPartialTitleAsync(string partialTitle)
+        {
+            return await dbContext.Books
+                .Where(b => EF.Functions.Like(b.Title, $"%{partialTitle}%"))
+                .Include(b => b.Author)
+                .ToListAsync();
+        }
+
+        public async Task UpdateBookWithCategoriesAsync(Book book, int[] categoriesId)
+        {
+            using (var transaction = await dbContext.Database.BeginTransactionAsync())
+            {
+                if (categoriesId != null)
+                {
+                    var oldCategories = await dbContext.BookCategories.Where(bc => bc.BookId == book.Id).ToArrayAsync();
+                    dbContext.BookCategories.RemoveRange(oldCategories);
+
+                    AddBookCategories(book.Id, categoriesId);
+                }
+
+                await UpdateAsync(book);
+
+                await transaction.CommitAsync();
+            }
+        }
+
+        private void AddBookCategories(int bookId, int[] categoriesId)
+        {
+            var categories = categoriesId.Select(i => new BookCategory { BookId = bookId, CategoryId = i });
+            dbContext.Set<BookCategory>().AddRange(categories);
         }
     }
 }
--- BookStore.Infrastructure.Services/Interfaces/IBookService.cs
+++ BookStore.Infrastructure.Services/Interfaces/IBookService.cs
@@ -9,5 +9,11 @@ namespace BookStore.Infrastructure.Services.Interfaces
     public interface IBookService : ICrudService<Book>
     {
         Task<int> GetBookLikesCountAsync(int id);
+
+        Task UpdateBookWithCategoriesAsync(Book book, int[] categoriesId);
+
+        Task CreateBookWithCategoriesAsync(Book book, int[] categoriesId);
+
+        Task<ICollection<Book>> GetBooksByPartialTitleAsync(string partialTitle);
     }
 }
