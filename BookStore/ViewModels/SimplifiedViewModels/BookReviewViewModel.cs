74216825b1441ccc84d1a21e78aba6c589e4ea98
pavlo.pustelnyk.pz.2017@lpnu.ua <pavlo.pustelnyk.pz.2017@lpnu.ua>
2020-01-25 15:25:58 +0200

    Add range constaint to book rating value

--- BookStore/ViewModels/SimplifiedViewModels/BookReviewViewModel.cs
+++ BookStore/ViewModels/SimplifiedViewModels/BookReviewViewModel.cs
@@ -9,12 +9,10 @@ namespace BookStore.WebAPI.ViewModels.SimplifiedViewModels
     public class BookReviewViewModel : BaseViewModel
     {
         [Required]
-        public int UserId { get; set; }
-
-        [Required]
         public int BookId { get; set; }
 
         [Required]
+        [Range(0, 5)]
         public int Rating { get; set; }
 
         public string Comment { get; set; }
